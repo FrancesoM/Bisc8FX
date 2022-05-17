7962503fa83e2d53f6c41c256ffff9c09ac85698
Francesco Maio <francesco.maio@aresys.it>
2022-05-17 12:01:25 +0200

    Revert to a decent project folder structure fo atan_sat IP

--- ip_repo/hls_ip/atan_saturation/atan_saturation_prj/script.tcl
+++ ip_repo/hls_ip/atan_saturation/atan_saturation_prj/script.tcl
@@ -11,7 +11,7 @@ open_solution "atan_saturation" -flow_target vivado
 set_part {xc7z020-clg400-1}
 create_clock -period 10 -name default
 config_export -format ip_catalog -rtl verilog -vivado_clock 10
-source "./atan_saturation_prj/atan_saturation/directives.tcl"
+source "./atan_saturation_prj/directives.tcl"
 #csim_design
 csynth_design
 #cosim_design
--- ip_repo/hls_ip/atan_saturation/build_project.bat
+++ ip_repo/hls_ip/atan_saturation/build_project.bat
@@ -1,1 +1,1 @@
-C:\Xilinx\Vitis_HLS\2021.1\bin\vitis_hls -f atan_saturation\atan_saturation_prj\script.tcl
+C:\Xilinx\Vitis_HLS\2021.1\bin\vitis_hls -f atan_saturation_prj\script.tcl
