c3f4d555f252aa99ad24e2db245313e4f19f618c
Francesco Maio <francesco.maio@aresys.it>
2022-05-17 14:09:23 +0200

    Fixed project names and structure

--- ip_repo/hls_ip/digital_setup/digital_setup_prj/digital_setup_prj/script.tcl
+++ ip_repo/hls_ip/digital_setup/digital_setup_prj/digital_setup_prj/script.tcl
@@ -5,16 +5,16 @@
 ############################################################
 open_project digital_setup_prj
 set_top digital_setup
-add_files srcs/atan_lut_almost_linear.h
-add_files srcs/digital_setup.cpp
 add_files srcs/digital_setup.h
+add_files srcs/digital_setup.cpp
+add_files srcs/atan_lut_almost_linear.h
 add_files -tb srcs/digital_setup_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
-open_solution "digital_setup_solution" -flow_target vivado
+open_solution "digital_setup_prj" -flow_target vivado
 set_part {xc7z020-clg400-1}
 create_clock -period 10 -name default
 config_export -format ip_catalog -rtl verilog -vendor Francesco -version 1.0
-source "./digital_setup_prj/directives.tcl"
-#csim_design
+source "./digital_setup_prj/digital_setup_prj/directives.tcl"
+csim_design
 csynth_design
-#cosim_design
-export_design -rtl verilog -format ip_catalog -vendor "Francesco"
+cosim_design
+export_design -format ip_catalog -vendor "Francesco"
