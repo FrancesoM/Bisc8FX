f8155b0f975ca236d04553912fd3a2f65522d15e
Francesco Maio <francesco.maio@aresys.it>
2022-05-17 12:05:55 +0200

    Decent project folder struct also for digital_setup IP

--- ip_repo/hls_ip/digital_setup/build_project.bat
+++ ip_repo/hls_ip/digital_setup/build_project.bat
@@ -1,1 +1,1 @@
-C:\Xilinx\Vitis_HLS\2021.1\bin\vitis_hls -f digital_setup\digital_setup_prj\script.tcl
+C:\Xilinx\Vitis_HLS\2021.1\bin\vitis_hls -f digital_setup_prj\script.tcl
--- ip_repo/hls_ip/digital_setup/digital_setup_prj/script.tcl
+++ ip_repo/hls_ip/digital_setup/digital_setup_prj/script.tcl
@@ -5,15 +5,15 @@
 ############################################################
 open_project digital_setup
 set_top digital_setup
-add_files digital_setup/srcs/atan_lut_almost_linear.h
-add_files digital_setup/srcs/digital_setup.cpp
-add_files digital_setup/srcs/digital_setup.h
-add_files -tb digital_setup/srcs/digital_setup_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files srcs/atan_lut_almost_linear.h
+add_files srcs/digital_setup.cpp
+add_files srcs/digital_setup.h
+add_files -tb srcs/digital_setup_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "digital_setup_prj" -flow_target vivado
 set_part {xc7z020-clg400-1}
 create_clock -period 10 -name default
 config_export -format ip_catalog -rtl verilog -vendor Francesco -version 1.0
-source "./digital_setup/digital_setup_prj/directives.tcl"
+source "./digital_setup_prj/directives.tcl"
 csim_design
 csynth_design
 cosim_design
